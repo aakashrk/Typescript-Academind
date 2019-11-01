8afb5509c7d8afa014068109afa3442cbf26999d
aakashrk <aakashrkhanvilkar@gmail.com>
2019-11-01 18:43:55 +0530

    Lesson 9. Numbers & Booleans

--- app.ts
+++ app.ts
@@ -1,3 +1,11 @@
 //string
 let myName='max'
 // myName=28
+
+//number
+let myAge =27.5 //No diff between int and float
+
+//boolean
+
+let hasHobbies = false
+//hasHobbies=1    // error because first you assign a boolean value and then trying to assign it a number
